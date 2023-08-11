91e470d2cb8e2a217b858ceecdbdb5965947410e
Gabriel <gabwisniewski@gmail.com>
2023-08-11 01:12:37 -0300

    adicionar funcoes de inputs diferentes

--- switch_case_bebida.js
+++ switch_case_bebida.js
@@ -1,2 +1,12 @@
 let bebida = "";
 const str_frase_bebida = "Qual bebida você quer?";
+
+function pegarValorPorBrowserPrompt(){                              //pegar input em um browser qualquer..
+    let str_bebida = window.prompt(str_frase_bebida);               //usando window.prompt
+    return bebida;
+}
+
+function pegarValorPorReadline(){                                   //pegar input por readline-sync... 
+    const readlineSync = require('readline-sync');                  //executando em Node.js
+    let str_bebida = readlineSync.question(str_frase_bebida);
+}
