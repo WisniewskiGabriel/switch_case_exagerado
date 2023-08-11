86aa12eebc522deee89fc2fabab48e6f1d4fe550
Gabriel <gabwisniewski@gmail.com>
2023-08-11 02:58:23 -0300

    switch do metodo de input concluido

--- switch_case_bebida.js
+++ switch_case_bebida.js
@@ -1,40 +1,48 @@
-const readlineSync = require('readline-sync');                  //executando em Node.js
+const readlineSync = require('readline-sync');                              
 let bebida = "";
 
 bebida = pegarValor();
-console.log(bebida);
 
-function pegarValor(){
-    let str_bebida = "";
-    switch(true){
-        case isEnvironmentNode():
-            str_bebida = pegarValorPorReadline();
+function pegarValor(){                                                      // função handler para chamada do... 
+    let str_bebida = "";                                                    // tipo de input correto
+                                                                            // - - - - - - - - - - - - - - - - - - - - - 
+    switch(true){                                                           // esse switch case é um pouco...
+        case isEnvironmentNode():                                           // atípico, porque usa uma assinatura...
+            str_bebida = pegarValorPorReadline();                           // true, sendo assim, qualquer case onde...
+            break;                                                          // valor função que retorno true será...
+        case isEnvironmentBrowser():                                        //  executada imediatamente
+            str_bebida = pegarValorPorBrowserPrompt();                      // - - - - - - - - - - - - - - - - - - - - - 
             break;
-    }
+        default:
+            console.log("De algum modo bizarro esta "+
+            "execução não está no Node "+
+            "e nem no Browser...")
+        }
+
     return str_bebida;
 }
 
-function pegarValorPorBrowserPrompt(){                              //pegar input em um browser qualquer..
-    let str_bebida = window.prompt("Qual bebida voce quer?");               //usando window.prompt
-    return bebida;
+function pegarValorPorBrowserPrompt(){                                      // pegar input em um browser qualquer..
+    let str_bebida = window.prompt("Qual bebida voce quer?");               // usando window.prompt
+    return bebida;                                                          // - - - - - - - - - - - - - - - - - - - - - 
 }
 
-function pegarValorPorReadline(){                                   //pegar input por readline-sync... 
-    let str_bebida = readlineSync.question('Qual bebida voce quer?');
-    return str_bebida;
+function pegarValorPorReadline(){                                           // pegar input por readline-sync... 
+    let str_bebida = readlineSync.question('Qual bebida voce quer?\n');     // executando em Node.js no terminal
+    return str_bebida;                                                      // - - - - - - - - - - - - - - - - - - - - - 
 }
 
-function atribuirValorDefaultSeBebibaEstiverUndefined(){            //nome de função desnecessariamente grande...
-    return "Chá";                                                   //tão desnecessaria quanto ela mesma 
-}
+function atribuirValorDefaultSeBebibaEstiverUndefined(){                    // nome de função desnecessariamente grande...
+return "Chá";                                                               // tão desnecessaria quanto ela mesma 
+}                                                                           // - - - - - - - - - - - - - - - - - - - - - 
 
-function isEnvironmentBrowser(){                                    // checa se o ambiente onde esse código...
-    return (typeof window !== 'undefined' &&                        // está rodando é um browser e...
-            typeof document!== 'undefined');                        // retorna uma booleana
-}
+function isEnvironmentBrowser(){                                            // checa se o ambiente onde esse código...
+    return (typeof window !== 'undefined' &&                                // está rodando é um browser e...
+            typeof document!== 'undefined');                                // retorna uma booleana
+}                                                                           // - - - - - - - - - - - - - - - - - - - - - 
 
-function isEnvironmentNode(){                                       // similar a função...
-    return (typeof global !== 'undefined'                           //'i s E n v i r o n m e n t B r o w s e r',...
-            && typeof process !== 'undefined'                       //mas checa se o ambiente é o Node
-            && typeof require !== 'undefined')                      //e retorna uma booleana
-}
+function isEnvironmentNode(){                                               // similar a função...
+    return (typeof global !== 'undefined'                                   // 'i s E n v i r o n m e n t B r o w s e r',...
+            && typeof process !== 'undefined'                               // mas checa se o ambiente é o Node...
+            && typeof require !== 'undefined')                              // e retorna uma booleana
+}                                                                           // - - - - - - - - - - - - - - - - - - - - -    
